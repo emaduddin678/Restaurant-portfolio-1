06fa405d3f71cfe0d50b43f5386c554770696f19
emaduddin678 <emaduddin678@gmail.com>
2024-02-01 00:43:36 +0600

    changes for speed up

--- js/main.js
+++ js/main.js
@@ -82,13 +82,3 @@ var swiper = new Swiper(".review-slider", {
   },
   loop: true,
 });
-
-function loader() {
-  document.querySelector(".loader-container").classList.add("fade-out");
-}
-
-function fadeOut() {
-  setInterval(loader, 3000);
-}
-
-window.onload = fadeOut;
